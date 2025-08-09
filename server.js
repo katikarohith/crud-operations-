825ee0dbad78030aa6416b428c46070b13840861
katikarohith <apnacollege62@gmail.com>
2025-08-09 05:59:23 +0530

    added

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname,"public")));
-const port= process.env.PORT ||3000
+const port= process.env.PORT ||8080
 
 
 let getUser = () => {
